4e4ebd73a6b07e25117b42dba541d7d5b99fed17
Bidursahoo <bibhabsahoobidur@gmail.com>
2023-09-18 23:58:06 +0530

    update verifiation

--- Routes/tasks.js
+++ Routes/tasks.js
@@ -68,19 +68,6 @@ router.put("/:id" , [auth , admin] , (req,res)=>{
 })
 
 
-//verify authentication token
 
-router.get("/verify" , auth , (req,res)=>{
-    if (req.user) {
-        res.status(200).json({
-          message: "Token verified successfully",
-          name: req.user.name
-        });
-      } else {
-        res.status(401).json({
-          message: "Token verification failed",
-        });
-      }
-})
 
 module.exports = router;
