41fd75c2af61407be3378d61b3a408341bfcf618
Bidursahoo <bibhabsahoobidur@gmail.com>
2023-09-19 01:16:40 +0530

    reverse

--- Routes/tasks.js
+++ Routes/tasks.js
@@ -68,6 +68,19 @@ router.put("/:id" , [auth , admin] , (req,res)=>{
 })
 
 
+//verify authentication token
 
+router.get("/verify" , auth , (req,res)=>{
+    if (req.user) {
+        res.status(200).json({
+          message: "Token verified successfully",
+          name: req.user.name
+        });
+      } else {
+        res.status(401).json({
+          message: "Token verification failed",
+        });
+      }
+  })
 
 module.exports = router;
